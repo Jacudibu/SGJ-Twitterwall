d991a64f57f293618c8642bafb714bc9fd9b39f2
Jacudibu <jacudibu@gmail.com>
2017-05-29 21:52:40 +0200

    Removed twitter api keys. No need to try them out, those are now invalid. :P

--- Assets/Scripts/Twitter/APIKeys.cs
+++ Assets/Scripts/Twitter/APIKeys.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 namespace Twitter
 {
+    // Put your Twitter API Keys here!
     public class APIKeys
     {
-        public static string authConsumerKey = "0gcc7aB20ihxhfdAocqrIePpX";
-        public static string authConsumerSecret = "vatHhY1DlO50SwJnRjYbnvivt2uZAuZDqctt6iVecC3UvfvVSv";
+        public static string authConsumerKey = "";
+        public static string authConsumerSecret = "";
         public static string authToken = "";
         public static string authTokenSecret = "";
     }
